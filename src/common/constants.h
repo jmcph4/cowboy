851b94f2337477ec290df301aeac0ab48fcb0433
jmcph4 <jmcph4.github@gmail.com>
2020-06-30 11:29:57 +1000

    Implement basic argument parsing for hub program
    This commit implements the basic argument parsing for the hub.

---
+++ src/common/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#define MAX_NUM_PLAYERS 16      /* largely arbitrary */
+
+#endif /* CONSTANTS_H_ */
+
--- src/hub/main.c
+++ src/hub/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    printf("Hello, world!\n");
+#include <common/constants.h>
+
+int main(int argc, char** argv) {
+    if(argc != 2) { /* usage */
+        fprintf(stderr, "usage: hub num_players\n");
+        return EXIT_FAILURE;
+    }
+
+    size_t num_players = atol(argv[1]);
+
+    if(num_players > MAX_NUM_PLAYERS) { /* bounds check */
+        fprintf(stderr, "Too many players\n");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
